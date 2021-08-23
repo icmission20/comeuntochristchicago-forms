fdc2d7cdcb06f4ae1af6da3415c4566db7c4ab60
icmission20 <89320303+icmission20@users.noreply.github.com>
2021-08-22 19:30:46 -0500

    update script link

--- formscript.js
+++ formscript.js
@@ -11,7 +11,7 @@ function onLoad() {
 const form = document.forms['comeuntochrist-form'];
 form.addEventListener('submit', e => {
 	console.log('test');
-	const scriptURL = "https://script.google.com/macros/s/AKfycbwzM0Ki3NNPH5UfEkjT2Ho3L-VMYYo3fOqzehkQ89wYsLVgzJjyg2mrPEzIeFLx861U/exec";
+	const scriptURL = "https://script.google.com/macros/s/AKfycbx2cUQpyDu5gSREiczUlRYVOelNGQ1SUAor3NAUd7XSrK8bjVvrscYwXkkaciJ-UqQ_/exec";
 	e.preventDefault();
 	fetch(scriptURL, {
 		method: 'POST',
