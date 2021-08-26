3500cd974aca998690875dc816c32614c686b96f
icmission20 <89320303+icmission20@users.noreply.github.com>
2021-08-26 18:18:09 -0500

    Added JS to hide the button when it is clicked to avoid duplicate submissions.
    document.getElementsByClassName("formButton")[0].style.display = 'none';

--- formscript.js
+++ formscript.js
@@ -10,7 +10,7 @@ function onLoad() {
 }
 const form = document.forms['comeuntochrist-form'];
 form.addEventListener('submit', e => {
-	console.log('test');
+	document.getElementsByClassName("formButton")[0].style.display = 'none';
 	const scriptURL = "https://script.google.com/macros/s/AKfycbw7ff_Vh6f0CXqOXkfRxCnDYuRvIdyIhEOjLa8E_D1S2Rw3-YH23SK2tP-P1UaGJ6mv/exec";
 	e.preventDefault();
 	fetch(scriptURL, {
@@ -19,5 +19,5 @@ form.addEventListener('submit', e => {
 	}).then(response => console.log('Success!', response)).catch(error => console.error('Error!', error.message));
 	setTimeout(function() {
 		window.parent.postMessage("redirect", "*");
-	}, 1000);
+	}, 1200);
 })
